b0ffc96687f50054998a048bca46e5a42ddf9bb0
Srirajkumar3 <rajkumardavu1-@gmail.com>
2024-06-05 09:00:47 +0530

    Updated Account.java to include feature X

--- Account.java
+++ Account.java
@@ -1,1 +1,69 @@
+import java.util.Random;
+
+public class Account {
+	private int accountBalance;
+	private int accountNumber;
+	private String accountType;
+	private String dateOfJoin;
+	public Account() {
+		
+	}
+	public Account(String accountType, String dateOfJoin) {
+		this.accountNumber = genAcNo();
+		this.dateOfJoin = dateOfJoin;
+		this.accountType = accountType;
+		this.accountBalance=genAcBal();
+		
+	}
+//	genAc
+	private int genAcNo() {
+		
+		Random R = new Random();
+		return R.nextInt(10000000,99999999);
+	}
+	
+// 
+private int genAcBal() {
+		
+		Random R = new Random();
+		return R.nextInt(10000,99999);
+	}
+	
+	
+	public int getAccountBalance() {
+		
+		return accountBalance;
+	}
+	public void setAccountBalance(int accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber() {
+		this.accountNumber = genAcNo();
+	}
+	public String getAccountType() {
+		return accountType;
+	}
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	public String getDateOfJoin() {
+		return dateOfJoin;
+	}
+	public void setDateOfJoin(String dateOfJoin) {
+		this.dateOfJoin = dateOfJoin;
+	}
+	
+	public void display() {
+		System.out.println("Account balance : "+accountBalance);
+		System.out.println("Account Type : "+accountType);
+		System.out.println("Account Number : "+accountNumber);
+		System.out.println("Date of join : "+dateOfJoin);
+	}
+	
+	
+
+}
 
